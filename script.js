df52e583564cc72850e75996c1c43d27eb3db910
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-09-09 10:36:50 +1000

    add cyber diffusion site

--- script.js
+++ script.js
@@ -1,85 +1,53 @@
 const data = {
   author: {
-    name: "Selkie Myth",
-    penName: "Selkie Myth",
-    bio: "Author of Beneath The Dragon Eye Moons.",
-    description:
-      "Join Elaine as she travels through the world of Pallos, with magic, monsters, and a global system everyone can tap into. ",
+    name: "Cyber-Diffusion",
+    penName: "AI Discord Bot",
+    bio: "A discord bot for image generation using Stable Diffusion and much more",
+    description: `
+Features:
+- Decentralised Compute, Open source, and scales with users.
+
+- Free for use
+
+- Upscaling with RealESRGAN
+
+- Max Resolution: 512x1024
+
+- Post Images directly to Twitter
+
+- Img2Img
+
+- Add to your own server for a small donation (1$ on patreon)!
+      `,
     image:
-      "https://cdn.glitch.global/af094f8f-cb9e-4e46-aef7-ee8b98c8965b/20890132.jpg?v=1657175829330",
+      "https://cdn.discordapp.com/attachments/1003896143186493440/1017586854859649065/generation.png",
   },
-  books: [
-    {
-      link: "https://www.amazon.com/gp/product/B08NWJMXXV",
-      image: "https://m.media-amazon.com/images/I/41KLgX1qPIL.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B08QMPDDKL",
-      image: "https://m.media-amazon.com/images/I/41sDoV-2AZL._SY300_.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B08Y5DSDQF",
-      image: "https://m.media-amazon.com/images/I/51R5hmfEWWL._SY300_.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B094YTHBML",
-      image: "https://m.media-amazon.com/images/I/51L1Nhhl0eL._SY300_.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B09GTTY8G8",
-      image: "https://m.media-amazon.com/images/I/417Uv0pZfRL._SY300_.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B09Q2SS1BT",
-      image: "https://m.media-amazon.com/images/I/510Wxp+OzjL._SY300_.jpg",
-    },
-    {
-      link: "https://www.amazon.com/gp/product/B09ZPQBW2C",
-      image: "https://m.media-amazon.com/images/I/51lwRCeLUtL._SY300_.jpg",
-    },
-  ],
+
   artwork: [
     {
       image:
-        "https://cdn.discordapp.com/attachments/753391562062299149/881607410840662046/Lunkat_transparent_bg.png",
+        "https://cdn.discordapp.com/attachments/1003896143186493440/1017589169138171946/generation.png",
     },
     {
       image:
-        "https://images-ext-1.discordapp.net/external/23tpm4rXivER2OU4T_vlTtyrR5Z53IBBaN0IMvVyUgE/https/i.imgur.com/ubYOt4z.png?width=1093&height=660",
+        "https://cdn.discordapp.com/attachments/989166996866359330/1017414944536416317/generation.png",
     },
     {
       image:
-        "https://images-ext-1.discordapp.net/external/IiBwjxfQKsyrPOhSGJddooRemZXo2jIzSO8nqDZVX6U/https/i.imgur.com/TzrWOsL.jpg?width=660&height=660",
+        "https://cdn.discordapp.com/attachments/1003896143186493440/1017292576350535690/buffer.png",
     },
     {
       image:
-        "https://images-ext-2.discordapp.net/external/zsEVfuPsfY-uO-Ue8PD_0mw_dYArEC0wNX3QSTvdr5Q/https/i.imgur.com/tKOMy2O.png?width=466&height=659",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/933032744282357830/933052857568067644/SPOILER_phoenix_1.jpg",
-      link: "/auri",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/751244061104406680/804138261689532476/Val_1.jpg?width=934&height=660",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/753391562062299149/815164914707595274/image0.jpg",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/753391562062299149/913214632200654858/Elaine_Violet.jpg?width=466&height=659",
+        "https://cdn.discordapp.com/attachments/989166996866359330/1016804367078260736/generation.png",
     },
   ],
   links: {
-    patreon: "https://www.patreon.com/SelkieMyth",
-    royalroad: "https://www.royalroad.com/profile/174291",
-    discord: "https://discord.gg/QNFpAk3p",
+    patreon: "https://www.patreon.com/unexplored_horizons",
+    discord: "https://discord.gg/eZqw6gMhf6",
+    github: "https://github.com/harrisonvanderbyl/ravenbot-ai",
   },
   background:
-    "https://cdn.glitch.global/af094f8f-cb9e-4e46-aef7-ee8b98c8965b/final_banner.jpg?v=1657176560875",
+    "https://cdn.discordapp.com/attachments/1003896143186493440/1017565354714726471/generation.png",
 };
 
 // onload
@@ -94,7 +62,20 @@ window.onload = function () {
 
   // set up the description
   const description = document.getElementById("description");
-  description.innerHTML = data.author.description;
+  description.innerHTML =
+    "<p>" +
+    data.author.description
+      .split("\n")
+      .map(
+        (m) =>
+          "<div style='margin-top:20px;margin-left:" +
+          (m.split("-")[0] ?? "").split(" ").length * 10 +
+          "px'>" +
+          m +
+          "</div>"
+      )
+      .join("\n") +
+    "</p>";
 
   // set up the pen name
   const penName = document.getElementsByClassName("penName");
@@ -107,16 +88,6 @@ window.onload = function () {
   for (let i = 0; i < image.length; i++) {
     image[i].src = data.author.image;
     image[i].href = data.author.image;
-  }
-
-  // set up the books
-  const books = document.getElementById("bookholder");
-  for (let i = 0; i < data.books.length; i++) {
-    const book = document.createElement("a");
-    book.href = data.books[i].link;
-    book.target = "_blank";
-    book.innerHTML = `<img src="${data.books[i].image}">`;
-    books.appendChild(book);
   }
 
   // set up the artwork
